acf59878465e6f4199a63f546b6bd593f7571dbc
Ivan Borvanov <ivan.borvanov@mail.ru>
2021-01-28 20:57:07 +0300

    heroku start

--- js/thebrvn.js
+++
@@ -1,6 +0,0 @@
-function myMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(55.16634441762963, 30.21802474023603),
-        zoom:10,
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp)};
